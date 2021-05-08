b65b8a5e768cfa766aee4bbb0c005b4bb4a63705
fmeurou <fmeurou@wapp6.com>
2021-05-08 17:12:25 +0200

    Use locMemCache for testing, not redis

--- tests/settings.py
+++ tests/settings.py
@@ -343,8 +343,7 @@ DATABASES = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
